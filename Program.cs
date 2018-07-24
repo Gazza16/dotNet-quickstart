0fe46b34bec20eaab68d58208bb170b8eda7710e
Gaston Mollica <mollica.gaston@gmail.com>
2018-07-24 12:33:40 +1000

    orders moved to a method

--- Program.cs
+++ Program.cs
@@ -4,12 +4,53 @@ namespace myApp
 {
     class Program
     {
+
+      static void WorkingWithIntegers() {
+        int a = 18;
+        int b = 6;
+        int c = a + b;
+          Console.WriteLine(c);
+
+        c = a - b;
+          Console.WriteLine(c);
+          c = a * b;
+          Console.WriteLine(c);
+          c = a / b;
+          Console.WriteLine(c);
+
+          c = a + b - 12 * 17;
+          Console.WriteLine(c);
+
+      }
+
+        static void orderPrecedence()
+        {
+          int a = 5;
+          int b = 4;
+          int c = 2;
+          int d = a + b * c;
+          Console.WriteLine(d);
+
+          d = (a  + b) * c;
+          Console.WriteLine(d);
+
+          d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+          Console.WriteLine(d);
+
+          int e = 7;
+          int f = 4;
+          int g = 3;
+          int h = (e  + f) / g;
+          Console.WriteLine(h);
+        }
+
         static void Main(string[] args)
         {
-          int a = 18;
-          int b = 6;
-          int c = a + b;
-          Console.WriteLine(c);
+              WorkingWithIntegers();
+
+              orderPrecedence();
+
+
         }
     }
 }
